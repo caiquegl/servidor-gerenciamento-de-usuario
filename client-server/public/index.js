24c6fc595cb0a96364bf361082e7fa8e9cce8257
caiquegl <arq.caique@hotmail.com>
2020-03-05 14:01:13 -0300

    criador feito e rodando

---
+++ client-server/public/index.js
@@ -0,0 +1,1 @@
+let userControler = new UserControler("form-user-create","form-user-update", "table-users");
---
+++ client-server/public/models/User.js
@@ -0,0 +1,182 @@
+class User {
+
+    constructor(name, gender, birth, country, email, password, photo, admin){
+
+        this._id;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
+
+    }
+
+    get id(){
+        return this._id;
+
+    }
+
+    get register(){
+        return this._register;
+
+    }
+    get name(){
+        return this._name;
+
+    }
+    get gender(){
+        return this._gender;
+
+    }
+    get birth(){
+        return this._birth;
+
+    }
+    get country(){
+        return this._country;
+
+    }
+    get email(){
+        return this._email;
+
+    }
+    get password(){
+        return this._password;
+
+    }
+    get photo(){
+        return this._photo;
+
+    }
+    get admin(){
+        return this._admin;
+
+    }
+
+    // ------------------------ set of user -----------------------
+
+    
+    set register(value){
+        this._register = value;
+
+    }
+    set name(value){
+        this._name = value;
+
+    }
+    set gender(value){
+        this._gender = value;
+
+    }
+    set birth(value){
+        this._birth = value;
+
+    }
+    set country(value){
+        this._country = value;
+
+    }
+    set email(value){
+        this._email = value;
+
+    }
+    set password(value){
+        this._password = value;
+
+    }
+    set photo(value){
+        this._photo = value;
+
+    }
+    set admin(value){
+        this._admin = value;
+
+    }
+    
+    loadFromJSON(json){
+
+        for (let name in json){
+
+            switch(name){
+
+            case "_register":
+                this[name] = new Date(json[name]);
+
+            break;
+            default:
+                if (name.substring(0, 1) === "_") this[name] = json[name]
+
+            }
+
+           
+
+        }
+
+    }
+
+    static getUsersStorage(){
+
+       return HttpRequest.get("/users");
+    }
+
+
+    toJSON(){
+
+        let json = {};
+
+        Object.keys(this).forEach(key =>{
+
+            if (this[key] !== undefined) json[key] = this[key];
+
+        });
+
+        return json;
+
+    }
+
+    save(){
+
+        return new Promise((resolve, reject)=>{
+
+            let promise;
+
+        if(this.id){
+
+           promise = HttpRequest.put(`/users/${this.id}`, this.toJSON())
+
+        }else{
+
+            promise = HttpRequest.post(`/users`, this.toJSON())
+
+
+        }
+
+        promise.then(data => {
+
+            this.loadFromJSON(data);
+
+            resolve(this);
+
+        }).catch(e=>{
+
+            reject(e);
+
+        });
+
+        });      
+
+    }
+
+        remove(){
+
+           return HttpRequest.delete(`/users/${this.id}`)
+
+
+        }
+
+
+}
